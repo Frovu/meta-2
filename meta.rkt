00c4cee45b9463ec30d495efc008482be00bb6be
Frovy <sfrovis@gmail.com>
2020-10-13 04:04:42 +0300

    somewhat working racket metacompilation

---
+++ meta.rkt
@@ -0,0 +1,76 @@
+#lang racket
+
+(provide err)
+(provide skip-whitespace)
+(provide read-str)
+(provide read-num)
+(provide read-id)
+(provide test)
+(provide output)
+(provide while-ok)
+(provide buffer)
+
+(current-input-port (open-input-file "test"))
+(current-output-port (open-output-file "test.rkt" #:exists 'replace))
+
+(define buffer "") ; input token buffer
+(define (err)
+	(display "error occured")
+	(exit 1))
+(define (skip-whitespace)
+	(let ([ch (peek-char )])
+		(unless (eof-object? ch)
+			(when (char-whitespace? ch)
+				(read-char )
+				(skip-whitespace)))))
+
+(define (read-it check res)
+	(let ([ch (peek-char)])
+		(if (check ch) (begin (read-char)
+			(read-it check (string-append res (string ch))))
+			(begin (set! buffer res) res))))
+(define (read-str)
+	(skip-whitespace)
+	(let ([ch (peek-char)])
+		(if (eof-object? ch) #f
+			(if (char=? ch #\')
+				(begin (read-char)
+					(let ([a (string-replace (string-append
+						(read-it (lambda (c) (not (char=? c #\'))) (string ch))
+						(string (read-char))) "'" "\"")])
+						(set! buffer a) a))
+				#f))))
+(define (read-num)
+	(skip-whitespace)
+	(let ([ch (peek-char)])
+		(if (eof-object? ch) #f
+			(if (char-numeric? ch)
+				(read-it (lambda (c) (char-numeric? c)) (string))
+				#f))))
+(define (read-id)
+	(skip-whitespace)
+	(let ([ch (peek-char)])
+		(if (eof-object? ch) #f
+			(if (char-alphabetic? ch)
+				(read-it (lambda (c)
+						(or (char=? c #\_) (char-alphabetic? c) (char-numeric? c)))
+					(string))
+				#f))))
+
+(define (test str)
+	(skip-whitespace)
+	(let ([peek-s (peek-string (string-length str) 0)])
+		(if (eof-object? peek-s) #f
+			(if (string=? str peek-s)
+				(read-string (string-length str))
+				#f))))
+
+(define (output . args)
+	(printf (string-join args "")))
+
+(define (while-rec check i)
+	(cond [(> i 10) #f]
+		[(check) (while-rec check (add1 i))]
+		[else #t]))
+(define (while-ok check)
+  (while-rec check 0))
---
+++ meta3l.rkt
@@ -0,0 +1,88 @@
+#lang racket
+(require "meta.rkt")
+
+(define (e-output)
+	(cond
+		[(if (test "<") (begin
+			(output "(output")
+			(while-ok (lambda () (cond
+				[(if (test "*") (begin
+					(output " buffer") #t) #f)]
+				[(if (read-str) (begin
+					(output " " buffer) #t) #f)]
+				[else #f])))
+			(unless (test ">") (err))
+			(output ")")) #f)]
+		[else #f]))
+
+(define (e-exp_atom)
+	(cond
+		[(if (read-id) (begin
+			(output "(e-" buffer ")") #t) #f)]
+		[(if (read-str) (begin
+			(output "(test " buffer ")") #t) #f)]
+		[(if (test ".ID") (begin
+			(output "(read-id)") #t) #f)]
+		[(if (test ".NUMBER") (begin
+			(output "(read-num)") #t) #f)]
+		[(if (test ".STRING") (begin
+			(output "(read-str)") #t) #f)]
+		[(if (test ".EMPTY") #t #f)]
+		[(if (test "(") (begin
+			(e-expression) (unless (test ")") (err)) #t) #f)]
+		[(if (test "$") (begin
+			(output "(while-ok (lambda () ")
+			(e-exp_atom)
+			(output "))") #t) #f)]
+		[else #f]))
+
+(define (e-exp_body)
+	(output "\n\t\t\t")
+	(while-ok (lambda () (cond
+		[(if (e-output)  (output "\n\t\t\t") #f)]
+		[(if (e-exp_atom)(output "\n\t\t\t") #f)]
+		[else #f])))
+	#t)
+
+(define (e-exp_option)
+	(cond
+		[(if (e-output) (begin
+			(output "(begin ")
+			(e-exp_body)
+			(output " #t)") #t) #f)]
+		[else
+			(output "(if ")
+			(e-exp_atom)
+			(output " (begin ")
+			(e-exp_body)
+			(output " #t) #f)") #t]))
+
+(define (e-expression)
+	(output "(cond\n\t\t[")
+	(e-exp_option)
+	(output "]")
+	(while-ok (lambda () (cond
+		[(if (test "/") (begin
+			(output "\n\t\t[")
+			(e-exp_option)
+			(output "]")) #f)]
+		[else #f])))
+	(output "\n\t\t[else #f])") #t)
+
+(define (e-statement)
+	(if (read-id) (begin
+	(output "(define (e-" buffer ")\n\t")
+	(test "=")
+	(e-expression)
+	(test ";")
+	(output ")\n") #t) #f))
+
+(define (e-compiler)
+	(output "#lang racket\n(require \"meta.rkt\")\n\n")
+	(while-ok e-statement)
+	(test ".SYNTAX")
+	(read-id)
+	(output "\n(unless (e-" buffer ") \"\\nfailed\")"))
+
+(unless (e-compiler) "\nfailed")
+(newline)
